92b32d5ff6e3987ec970befb06e73151c9421345
Mike Schaeffer <mike@mschaef.com>
2020-05-01 21:20:16 -0400

    Compute an additional set of numbers for a Risk strategy where the attacker stops pressing the attack when troop count falls below the number of defenders.

--- risk-battle.c
+++ risk-battle.c
@@ -5,10 +5,24 @@
 
 #include "mt19937.h"
 
-#define DEBUG_MSG(x)
+#define DEBUG 0
+#define TRACE 0
 
-#define SAMPLE_RUNS 1000000
+#define SAMPLE_RUNS 5000000
 #define TROOP_RANGE 20
+
+#if DEBUG
+#  define DEBUG_MSG(x) x
+# else
+#  define DEBUG_MSG(x)
+#endif
+
+#if TRACE
+#  define TRACE_MSG(x) x
+# else
+#  define TRACE_MSG(x)
+#endif
+
 
 char roll_dice() {
      long x = mt19937_int31();
@@ -23,7 +37,7 @@ void sort_step(char *a, char *b) {
           *a = temp;
      }
 }
-     
+
 void roll_hand(int die_rolled, int num_die, char dice[]) {
      for(int ii = 0; ii < num_die; ii++) {
           dice[ii] = (ii < die_rolled) ? roll_dice() : -1;
@@ -31,7 +45,7 @@ void roll_hand(int die_rolled, int num_die, char dice[]) {
 
      // num_die bounded by sorting network capacity.
      assert(num_die < 4);
-     
+
      sort_step(&dice[0], &dice[1]);
      sort_step(&dice[1], &dice[2]);
      sort_step(&dice[0], &dice[1]);
@@ -53,14 +67,14 @@ void evaluate(int *attackers, int *defenders) {
      roll_hand(min(3, *attackers), 3, a_die);
      roll_hand(min(2, *defenders), 3, d_die);
 
-     DEBUG_MSG(show_dice("attacker", a_die));
-     DEBUG_MSG(show_dice("defender", d_die));
+     TRACE_MSG(show_dice("attacker", a_die));
+     TRACE_MSG(show_dice("defender", d_die));
 
      for(int ii = 0; ii < 3; ii++) {
           if ((a_die[ii] == -1) || (d_die[ii] == -1)) {
                return;
           }
-          
+
           if (a_die[ii] > d_die[ii]) {
                *defenders = *defenders - 1;
           } else if (a_die[ii] <= d_die[ii]) {
@@ -69,50 +83,108 @@ void evaluate(int *attackers, int *defenders) {
      }
 }
 
-void simulate_games(int _attackers, int _defenders,
-                    double *attacker_win_prob,
-                    double *attacker_avg_losses,
-                    double *defender_avg_losses) {
-     
-     int attacker_wins = 0;
-     int defender_wins = 0;
+struct simulation_result {
+     int attacker_wins;
+     int defender_wins;
+     int draws;
+     int attacker_troop_losses;
+     int defender_troop_losses;
 
-     int attacker_troop_losses = 0;
-     int defender_troop_losses = 0;
-     
+     double attacker_win_prob;
+     double draw_prob;
+     double attacker_avg_losses;
+     double defender_avg_losses;
+};
+
+void compute_stats(struct simulation_result *result) {
+     result->attacker_win_prob = (double)result->attacker_wins / (double)SAMPLE_RUNS;
+     result->draw_prob = (double)result->draws / (double)SAMPLE_RUNS;
+
+     result->attacker_avg_losses = (double)result->attacker_troop_losses / (double)SAMPLE_RUNS;
+     result->defender_avg_losses = (double)result->defender_troop_losses / (double)SAMPLE_RUNS;
+}
+
+void update_result(char *label,
+                   struct simulation_result *result,
+                   int initial_attackers, int initial_defenders,
+                   int attackers, int defenders) {
+     result->attacker_troop_losses += (initial_attackers - attackers);
+     result->defender_troop_losses += (initial_defenders - defenders);
+
+     if (!attackers) {
+          DEBUG_MSG(printf("  %s defender wins\n", label));
+          result->defender_wins++;
+     } else if (!defenders) {
+          DEBUG_MSG(printf("  %s attacker wins\n", label));
+          result->attacker_wins++;
+     } else {
+          DEBUG_MSG(printf("  %s draw\n", label));
+          result->draws++;
+     }
+}
+
+void simulate_games(int initial_attackers, int initial_defenders,
+                    struct simulation_result *result,
+                    struct simulation_result *c_result) {
+
+     memset(result, 0, sizeof(struct simulation_result));
+     memset(c_result, 0, sizeof(struct simulation_result));
+
      for(int ii = 0; ii < SAMPLE_RUNS; ii++) {
           DEBUG_MSG(printf("\n"));
 
-          int attackers = _attackers;
-          int defenders = _defenders;
+          int attackers = initial_attackers;
+          int defenders = initial_defenders;
 
-          int count = 100;
+          int c_attackers = initial_attackers;
+          int c_defenders = initial_defenders;
 
-          while(attackers && defenders && count) {
-               evaluate(&attackers, &defenders);
+          int stopped = 0;
 
+          while(attackers && defenders) {
                DEBUG_MSG(printf("a: %d, d: %d\n", attackers, defenders));
 
-               count--;
+               if (attackers < defenders) {
+                    stopped = 1;
+               }
+
+               evaluate(&attackers, &defenders);
+
+               if (!stopped) {
+                    c_attackers = attackers;
+                    c_defenders = defenders;
+               }
           }
 
-          attacker_troop_losses += (_attackers - attackers);
-          defender_troop_losses += (_defenders - defenders);
-
-          if (!attackers) {
-               defender_wins++;
-          } else if (!defenders) {
-               attacker_wins++;
-          }
+          update_result("total", result, initial_attackers, initial_defenders, attackers, defenders);
+          update_result("cautious", c_result, initial_attackers, initial_defenders, c_attackers, c_defenders);
      }
 
-     DEBUG_MSG(printf("att/def wins: %d/%d\n", attacker_wins, defender_wins));
-     
-     *attacker_win_prob =
-          (double)attacker_wins / ((double)attacker_wins + (double)defender_wins);
+     DEBUG_MSG(printf("att/def wins: %d/%d (%d/%d)\n",
+                      result->attacker_wins, result->defender_wins,
+                      c_result->attacker_wins, c_result->defender_wins));
 
-     *attacker_avg_losses = (double)attacker_troop_losses / (double)SAMPLE_RUNS;
-     *defender_avg_losses = (double)defender_troop_losses / (double)SAMPLE_RUNS;
+
+     compute_stats(result);
+     compute_stats(c_result);
+}
+
+void sample(int attackers, int defenders) {
+     struct simulation_result result;
+     struct simulation_result c_result;
+
+     simulate_games(attackers, defenders, &result, &c_result);
+
+     printf("%d, %d, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f\n",
+            attackers, defenders,
+            result.attacker_win_prob,
+            result.draw_prob,
+            result.attacker_avg_losses,
+            result.defender_avg_losses,
+            c_result.attacker_win_prob,
+            c_result.draw_prob,
+            c_result.attacker_avg_losses,
+            c_result.defender_avg_losses);
 }
 
 int main(int argc, char *argv[]) {
@@ -120,15 +192,9 @@ int main(int argc, char *argv[]) {
 
      for(int attackers = 1; attackers < TROOP_RANGE; attackers++) {
           for(int defenders = 1; defenders < TROOP_RANGE; defenders++) {
-               double p_awin;
-               double avg_aloss;
-               double avg_dloss;
-
-               simulate_games(attackers, defenders, &p_awin, &avg_aloss, &avg_dloss);
-
-               printf("%d,%d,%0.4f,%0.4f,%0.4f\n", attackers, defenders, p_awin, avg_aloss, avg_dloss);
+               sample(attackers, defenders);
           }
      }
-     
+
      fprintf(stderr, "end run.\n");
 }
