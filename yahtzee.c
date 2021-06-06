f483b22662044a6dfa75cacaceeee90b1dbe4f84
Mike Schaeffer <mike@mschaef.com>
2021-06-06 09:41:40 -0400

    Add a simple strategy to roll for better chance hance hands.

--- yahtzee.c
+++ yahtzee.c
@@ -14,6 +14,14 @@ void roll_hand(char dice[NDICE]) {
      }
 }
 
+void improve_chance_hand(char dice[NDICE]) {
+     for(int ii = 0; ii < NDICE; ii++) {
+          if(dice[ii] < 4) {
+               dice[ii] = roll_dice();
+          }
+     }
+}
+
 void show_hand(char dice[NDICE]) {
      for(int ii = 0; ii < NDICE; ii++) {
           printf("%d", dice[ii]);
@@ -39,12 +47,14 @@ int main(int argc, char *argv[]) {
      init_mt19937(0);
 
      int scores[31];
+     int improved_scores[31];
 
      memset(scores, 0, sizeof(scores));
+     memset(improved_scores, 0, sizeof(improved_scores));
 
      char dice[NDICE];
 
-     for(int ii = 0; ii < 1000; ii++) {
+     for(int ii = 0; ii < 100000; ii++) {
           roll_hand(dice);
           DEBUG_MSG(show_hand(dice));
 
@@ -53,10 +63,17 @@ int main(int argc, char *argv[]) {
           DEBUG_MSG(printf("%d\n", score));
 
           scores[score]++;
+
+          improve_chance_hand(dice);
+          improve_chance_hand(dice);
+
+          score = hand_chance_score(dice);
+          
+          improved_scores[score]++;
      }
 
      for(int ii = 0; ii < 31; ii++) {
-          printf("%d, %d\n", ii, scores[ii]);
+          printf("%d, %d, %d\n", ii, scores[ii], improved_scores[ii]);
      }
      
      fprintf(stderr, "end run.\n");
